e0fa3a9621041cbd2808016499a8494968b39690
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-04-17 00:03:04 +0530

    adding intial version

---
+++ index.js
@@ -0,0 +1,26 @@
+var serand = require('serand');
+
+serand.on('token', 'info', function (id, token, done) {
+    if (!done) {
+        done = token;
+        token = null;
+    }
+    var options = {
+        method: 'GET',
+        url: '/apis/v/tokens/' + id,
+        headers: {
+            'X-Host': 'accounts.serandives.com'
+        },
+        dataType: 'json',
+        success: function (token) {
+            done(false, token);
+        },
+        error: function () {
+            done('permissions error');
+        }
+    };
+    if (token) {
+        options.headers['Authorization'] = 'Bearer ' + token;
+    }
+    $.ajax(options);
+});
