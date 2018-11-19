580f7b103b7eb7c92f2faaa47c362ffaefaaee1b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-11-19 12:56:58 +0530

    updating

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var serand = require('serand');
 var utils = require('utils');
+var serand = require('serand');
 
-serand.on('token', 'info', function (id, token, done) {
+exports.findOne = function (id, token, done) {
     if (!done) {
         done = token;
         token = null;
     }
-    var options = {
-        method: 'GET',
-        url: utils.resolve('accounts:///apis/v/tokens/' + id),
-        dataType: 'json',
-        success: function (token) {
-            done(null, token);
-        },
-        error: function (xhr, status, err) {
-            done(err || status || xhr);
+    serand.cached('page', id, function (did) {
+        var options = {
+            method: 'GET',
+            url: utils.resolve('accounts:///apis/v/tokens/' + id),
+            dataType: 'json',
+            success: function (token) {
+                did(null, token);
+            },
+            error: function (xhr, status, err) {
+                did(err || status || xhr);
+            }
+        };
+        if (token) {
+            options.headers = options.headers || {};
+            options.headers['Authorization'] = 'Bearer ' + token;
         }
-    };
-    if (token) {
-        options.headers = options.headers || {};
-        options.headers['Authorization'] = 'Bearer ' + token;
-    }
-    $.ajax(options);
-});
+        $.ajax(options);
+    }, done);
+};
